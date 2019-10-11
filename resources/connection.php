294041fd2516e763b595c09946b2172a08599d25
node2begin <github@node2begin.php>
2019-10-11 07:45:15 -0700

    From Sir laptop

--- resources/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-	//mysql_connect("localhost","root","root") or die("Cannot connect to Server");
-	$con=mysqli_connect("localhost","root","root","club") or die("Connection not found and Cannot connect to database");
-	//mysql_select_db("club") or die("Cannot connect to database");
-?>
